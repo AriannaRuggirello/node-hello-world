6bfef66a1256cb494219b6d00fc6cc8df064e783
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-11-07 15:15:29 +0100

    bonus

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 
 // la avvio
 dotenv.config();
+
 // creao la porta 
 const port = process.env.PORT || 8000;
 
@@ -16,8 +17,22 @@ const port = process.env.PORT || 8000;
 const server = http.createServer(function(req,res){
     // Devo specificare come il server deve rispondere
     // Invio dati in formato HTML
-    res.writeHead(200,{"Content-Type":"text/html"});
-    res.end('<h1>CIAO MONDO!</h1>');
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+    // creo un array di frasi
+    let phrases =[
+        "L'azione è la chiave fondamentale di ogni successo.",
+        "Sii il cambiamento che vuoi vedere nel mondo.",
+        "Non importa quanto tu vada piano, purché non ti fermi." ,
+        "Ogni giorno è una nuova opportunità. Prendila. Respira e inizia di nuovo." 
+    ]
+   // Genero un indice per selezionare una frase casuale dall'array
+    let i = Math.floor(Math.random() * phrases.length);
+
+    //frase dall'array uguale all'indice 
+    let phrase = phrases[i];
+    // stampo la frase
+    res.end(`<h1>La frase del giorno è: <br> ${phrase}</h1>`);
 });
 
 // avvio il server
